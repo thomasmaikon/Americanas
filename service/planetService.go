91e3a7c2bfde0458a863231ff321f956ebc8009d
thomas <thomasmaikon@hotmail.com>
2022-08-30 11:04:10 -0300

    Refact: planetService

--- service/planetService.go
+++ service/planetService.go
@@ -3,34 +3,36 @@ package service
 import "projeto/Americanas/model"
 
 var repository = FactoryDB{}.GetMongoDB()
+var repositoryRedis = FactoryDB{}.GetRedisDB()
 
 type ServicePlanet struct {
 }
 
-func (s ServicePlanet) CreateNewPlanet(newPLanet model.PLanet) bool {
+func (s ServicePlanet) CreateNewPlanet(newPLanet model.Planet) bool {
+	newPLanet.Films = repositoryRedis.FindByName(newPLanet.Name).Films
 	return repository.Add(newPLanet)
 }
 
-func (s ServicePlanet) Find(name string, id int64) []model.PLanet {
-	result := []model.PLanet{}
+func (s ServicePlanet) Find(name string, id string) []model.Planet {
+	result := []model.Planet{}
 	if name != "" {
 		return append(result, s.findByName(name))
 	}
-	if id != 0 {
+	if id != "" {
 		return append(result, s.findById(id))
 	}
 	return s.findAll()
 }
 
-func (s ServicePlanet) findByName(name string) model.PLanet {
+func (s ServicePlanet) findByName(name string) model.Planet {
 	return repository.FindByName(name)
 }
 
-func (s ServicePlanet) findAll() []model.PLanet {
+func (s ServicePlanet) findAll() []model.Planet {
 	return repository.FindAll()
 }
 
-func (s ServicePlanet) findById(id int64) model.PLanet {
+func (s ServicePlanet) findById(id string) model.Planet {
 	return repository.FindById(id)
 }
 
